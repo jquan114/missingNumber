059f0b8dfc1a077875c46964d7da904f54657267
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-01-21 12:02:35 -0500

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,8 @@
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+
+var missingNumber = function(nums) {
+        let expectedSum = (nums.length * (nums.length + 1)) / 2;
+    let actualSum = nums.reduce((acc, num) => acc + num, 0);
+    return expectedSum - actualSum;
+};
